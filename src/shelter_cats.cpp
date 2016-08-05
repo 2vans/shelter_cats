4e029a93a299fca4adfe58895100d0e586fb3ba0
unknown <mock.jablonski@gmail.com>
2016-08-05 13:26:22 +0100

    Initial Commit

---
+++ src/shelter_cats.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : shelter_cats.cpp
+// Author      : Michal Jablonski
+// Version     :
+// Copyright   : DeeR Corporation.
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
