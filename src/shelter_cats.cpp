9c079eee184f5c0349201a175f27256458175cfd
unknown <mock.jablonski@gmail.com>
2016-08-09 13:51:07 +0100

    Repairs the huge bug, adds overflow logic

--- src/shelter_cats.cpp
+++ src/shelter_cats.cpp
@@ -10,12 +10,14 @@
 using namespace std;
 
 int main() {
+	//declarations of variables
 	int space_count;
 	int menu_choice;
 	int cats_number = 0;
 	string delete_choice;
 
 	bool exit = false;
+	//first main loop
 	while (!exit) {
 		//Ask for the amount of space you have in your shelter
 		cout << "How much space do you have in shelter?" << endl;
@@ -31,9 +33,9 @@ int main() {
 		} else {
 			// Creates database with amount you submitted
 
-			string cat_database[space_count - 1][4];
+			string cat_database[space_count][4];
 
-			//main loop
+			//secend main loop
 			while (!exit) {
 				//simple menu
 				cout << "Space for cats: " << space_count << endl;
@@ -44,50 +46,78 @@ int main() {
 				cout << "2. Remove cat." << endl;
 				cout << "3. Status" << endl;
 				cout << "4. Exit" << endl;
+
 				//your input in menu
 				cin >> menu_choice;
+				//make sure that user iput is integer
+				if (cin.fail()) {
+					cin.clear();
+					std::cin.ignore(256, '\n');
+					cout << "Enter number" << endl;
 
+				}
+				// mennu logic
+				// choice 1. add cat
 				if (menu_choice == 1) {
 					if (cats_number >= space_count) {
-						cout << "you have to many cats, remove at least one." << endl;
+						//overflow logic
+						cout << "you have to many cats, remove at least one."
+								<< endl;
 					} else {
-					//adding cat
-					cout << endl;
-					cout << "Space for cats: " << space_count - cats_number
-							<< endl;
-					cout << endl;
-					cout << "1. Name:" << endl;
-					cin >> cat_database[cats_number][0];
-					cout << "2. Age:" << endl;
-					cin >> cat_database[cats_number][1];
-					cout << "3. Vaccines" << endl;
-					cin >> cat_database[cats_number][2];
-					cout << "4. Description" << endl;
-					cin >> cat_database[cats_number][3];
+						//adding cat logic
+						cout << endl;
+						cout << "Space for cats: " << space_count - cats_number
+								<< endl;
+						cout << endl;
+						//spcecifie name
+						cout << "1. Name:" << endl;
+						cin >> cat_database[cats_number][0];
+						//specifie age
+						cout << "2. Age:" << endl;
+						cin >> cat_database[cats_number][1];
+						// specifie vaccines
+						cout << "3. Vaccines" << endl;
+						cin >> cat_database[cats_number][2];
+						//specifie description
+						cout << "4. Description" << endl;
+						cin >> cat_database[cats_number][3];
 
-					//incrising number of cats
-					cats_number++;
+						//incrising number of cats
+						cats_number++;
 					}
 				} else
 				// kasowanie ostatniego kota
 				if (menu_choice == 2) {
-					cout
-							<< "Are you sure that you want to remove last added cat? (y/n)";
-					cin >> delete_choice;
-					if (delete_choice == "y") {
 
-						for (int j = 0; j < 4; j++) {
-							cat_database[cats_number][j] = "NULL";
-
-						}
-						cats_number--;
-					} else if (delete_choice == "y") {
-						cout << "Deletind stops." << endl;
+					// checking if yoy have a cat
+					if (cats_number <= 0) {
+						cout << "You have no cats in database" << endl;
 					} else {
-					}
-				} else
+						//make sure option
+						cout
+								<< "Are you sure that you want to remove last added cat? (y/n)"
+								<< endl;
 
-				if (menu_choice == 3) {
+						cin >> delete_choice;
+						if (delete_choice == "y") {
+							//deleting kat
+							for (int j = 0; j < 4; j++) {
+								cat_database[cats_number - 1][j] = "NULL";
+
+							}
+							cats_number--;
+
+						} else if (delete_choice == "n") {
+							//no option in deleting cat
+							cout << "Deleting stops." << endl;
+						} else {
+
+							cout << "Unknown command, please try again" << endl;
+						}
+					}
+					//comand status
+				} else if (menu_choice == 3) {
+					//print on screen list of cats
 					for (int i = 0; i < cats_number; i++) {
 						for (int j = 0; j < 4; j++) {
 							cout << cat_database[i][j] << " " << flush;
@@ -96,7 +126,7 @@ int main() {
 					}
 					cout << endl;
 				} else
-
+				//comand exit
 				if (menu_choice == 4) {
 					//exit the program
 					exit = true;
